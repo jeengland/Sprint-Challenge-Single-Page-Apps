2540fd3954c4b5597ba529d172fb9c4952a4cce7
unknown <jacob.e.england@gmail.com>
2020-02-28 10:15:01 -0600

    Added axios call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from 'axios';
 
 
 export default function App() {
+  const [ data, setData ] = useState([]);
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/')
+         .then((response) => {
+           setData(response.data.results);
+         })
+  }, [])
+  console.log(data);
   return (
     <main>
       <Header />
