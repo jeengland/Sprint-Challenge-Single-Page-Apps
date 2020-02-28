c1966e05e74982cd2905fcdc17256a359ee8ac1f
unknown <jacob.e.england@gmail.com>
2020-02-28 11:39:40 -0600

    MVP reached

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
+import styled from '@emotion/styled';
+
+const Search = styled.section`
+  margin-top: 2%;
+  display: flex;
+  justify-content: center;
+`
 
 export default function SearchForm(props) {
   return (
-    <section className='search-form'>
+    <Search className='search-form'>
       <input type='text' value={props.query} onChange={(event) => props.setQuery(event.target.value)} placeholder='Search by name'/>
-    </section>
+    </Search>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styled from '@emotion/styled';
+
+const Welcome = styled.section`
+  a {
+    display: block;
+    width: 10%;
+    padding: 1%;
+    margin: 2% auto;
+    background-color: turquoise;
+    box-shadow: 0 3px 3px rgba(0, 0, 0, 0.3), 0 3px 3px rgba(0, 0, 0, 0.22);
+    font-size: 1.5rem;
+    color: white;
+    text-decoration: none;
+    text-align: center;
+    &:hover {
+      background-color: paleturquoise;
+      color: black;
+    }
+    &:active {
+      box-shadow: none;
+    }
+  }
+`
 
 export default function WelcomePage() {
   return (
-    <section className='welcome-page'>
+    <Welcome className='welcome-page'>
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
@@ -13,6 +36,6 @@ export default function WelcomePage() {
         />
         <Link to='/characters'>Enter</Link>
       </header>
-    </section>
+    </Welcome>
   );
 }
